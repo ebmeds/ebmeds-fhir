d06d6ac3471e22ff4d2115b99e583e9a4ba6d4da
tomkinen <tomi.kinnunen@duodecim.fi>
2016-04-15 14:49:04 +0300

    Response FHIR conversion according to Case 3.

--- app/routes/api.js
+++ app/routes/api.js
@@ -2,9 +2,14 @@ var xml2js = require('xml2js');
 var jp = require('jsonpath');
 var ebmedsClient = require('../client/ebmeds.js');
 var ebmedsRequestTemplate = require('../client/request-template.json');
+var FHIRResponseTemplate = require('../client/response-template.json');
 var requestMockIncoming = require('../client/request-mock-incoming.json');
 var Patient = require('../domain/Patient');
 var Measurement = require('../domain/Measurement');
+var config = require('config');
+// Variable to find Finrisk -link from Experimental dataset
+var FinriskLink= config.get('finrisk.url');
+var resultFHIR = {};
 
 exports.healthCoaching = function(req, res) {
 
@@ -17,10 +22,20 @@ exports.healthCoaching = function(req, res) {
     ebmedsRequestTemplate.DSSRequest.System.Application.QueryID = activityInstance.valueString;
 
     var ebmedsRequest = new xml2js.Builder().buildObject(ebmedsRequestTemplate);
-    
     ebmedsClient.request(ebmedsRequest).then(function(response) {
         xml2js.parseString(response, function(err, result) {
-            res.json(result);
+            //Parsing result from EBMeDS engine, finding Finrisk Link from JSON Response
+            var resFormAssistantLinks = jp.query(result, '$..ExperimentalDataSet[?(@.DataSetName=="FormAssistantLinks")]')[0];
+            var resDataSetText = jp.query(resFormAssistantLinks, '$..DataSetText')[0];
+            //Getting just the link to Finrisk from the DataSetText
+            var resFinriskLink = JSON.stringify(resDataSetText);
+            resFinriskLink = resFinriskLink.split(FinriskLink).pop();
+            resFinriskLink = resFinriskLink.split("'")[0];
+            resFinriskLink = FinriskLink+resFinriskLink;
+            //Generating FHIR JSON response
+            resultFHIR = FHIRResponseTemplate;
+            resultFHIR.parameter[0].part[4].part[1].valueUri=resFinriskLink;
+            res.json(resultFHIR);
         });
     });
 };
