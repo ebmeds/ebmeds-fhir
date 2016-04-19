5b368adb5126216ece0bec29204bbae4b4888156
jukkasi <jukka.siivonen@solita.fi>
2016-04-19 11:13:32 +0300

    Added some documentation on index page

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,11 @@
 var pkg = require('../../package.json');
+var request1 = require('../client/request-mock-incoming-with-context.json');
+var request2 = require('../client/request-mock-incoming-without-context.json');
 
 exports.index = function(req, res) {
-    res.render('index', { version: pkg.version });
+    res.render('index', {
+        version: pkg.version,
+        request1: JSON.stringify(request1, null, 2),
+        request2: JSON.stringify(request2, null, 2)
+    });
 };
