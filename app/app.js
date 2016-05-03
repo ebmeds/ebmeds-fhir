e3ca8851ee38570d5de8ad2db197ac8f09c24f54
jukkasi <jukka.siivonen@solita.fi>
2016-05-03 09:28:10 +0300

    Simplified CORS setup

--- app/app.js
+++ app/app.js
@@ -10,6 +10,8 @@ var app = express();
 app.set('port', config.get('server.port'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
+
+app.use(cors());
 app.use(express.favicon());
 app.use(express.bodyParser());
 app.use(express.methodOverride());
@@ -19,10 +21,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.configure('production', function() { require('./production-configure')(app); });
 app.configure('development', function() { require('./development-configure')(app); });
 
-app.options('*', cors());
-
 app.get('/', routes.index);
-app.post('/api/health-coaching', cors(), api.healthCoaching);
+app.post('/api/health-coaching', api.healthCoaching);
 
 http.createServer(app).listen(app.get('port'), function() {
     console.log('Express server listening on port ' + app.get('port'));
