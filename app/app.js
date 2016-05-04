f820ce874b7727bc326004d16bebf3339bc20ebe
jukkasi <jukka.siivonen@solita.fi>
2016-05-04 10:42:09 +0300

    Upgrade Express 3 -> 4 so we are able to support content type application/json+fhir for JSON

--- app/app.js
+++ app/app.js
@@ -5,6 +5,7 @@ var http = require('http');
 var config = require('config');
 var path = require('path');
 var cors = require('cors');
+var bodyParser = require('body-parser-json');
 var app = express();
 
 app.set('port', config.get('server.port'));
@@ -12,14 +13,12 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
 app.use(cors());
-app.use(express.favicon());
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
+app.use(bodyParser.json({ type: 'application/*' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.configure('production', function() { require('./production-configure')(app); });
-app.configure('development', function() { require('./development-configure')(app); });
+process.env.NODE_ENV === "production" ?
+    require('./production-configure')(app) :
+    require('./development-configure')(app); // jshint ignore:line
 
 app.get('/', routes.index);
 app.post('/api/health-coaching', api.healthCoaching);
