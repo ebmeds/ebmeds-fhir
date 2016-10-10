c21d879d80a06f3ccb21a9220d3b4dcef65c2973
jukkasi <jukka.siivonen@solita.fi>
2016-10-10 14:13:19 +0300

    Fixed error logging middleware

--- app/middleware/error-logger.js
+++ app/middleware/error-logger.js
@@ -3,12 +3,12 @@ var winston = require('winston');
 module.exports = function() {
 
     var logger = new winston.Logger({
-        transports: new winston.transports.Console({
+        transports: [new winston.transports.Console({
             level: 'error',
             humanReadableUnhandledException: true,
             handleExceptions: true,
             json: false
-        })
+        })]
     });
 
     return function(err, req, res, next) {
--- app/production-configure.js
+++ app/production-configure.js
@@ -1,4 +1,4 @@
 module.exports = function(app) {
-    app.use(require('./middleware/error-logger'));
+    app.use(require('./middleware/error-logger')());
     app.use(require('./middleware/error-response')({ stacktrace: false }));
 };
