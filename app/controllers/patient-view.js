018f128c996c0018f8dc2700976428a578d16a50
jukkasi <jukka.siivonen@solita.fi>
2016-10-27 20:50:02 +0300

    Introduced patient-view API

---
+++ app/controllers/patient-view.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+var pipe = require('../transformers/pipe');
+
+router.post('/\\$cds-hook', function(req, res, next) {
+
+    var context = {
+        requestParams: req.query || {},
+        language: "en-US",
+        nation: "US",
+        cards: ["reminders", "guidelink"]
+    };
+
+    pipe.execute(req, res, next, context);
+});
+
+router.get('/\\$cds-hook-metadata', function(req, res, next) {
+
+    res.json({
+        "resourceType" : "Parameters",
+        "parameter" : [{
+            "name" : "name",
+            "valueString" : "EBMeDS patient view"
+        }, {
+            "name" : "description",
+            "valueString" : "EBMeDS patient view"
+        }, {
+            "name" : "activity",
+            "valueCoding" : {
+                "system" : "http://cds-hooks.smarthealthit.org/activity",
+                "code" : "patient-view"
+            }
+        }
+        ]
+    });
+});
+
+module.exports = router;
